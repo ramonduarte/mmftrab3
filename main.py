c024acaecd4929318f0a47e3e478320ae08c8488
Ramon Melo <ramonduarte@poli.ufrj.br>
2019-06-10 10:25:31 -0300

    Additional scraping methods.

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
 import math
-
-
-frame = pd.read_csv("csv/SI_D_ROPC.txt", sep="|", header=None, skiprows=1,
-                    parse_dates=[3], 
-                    names=["?", "stock", "stock type", "exercise date", "??", "???",
-                           "series", "????", "strike price", "?????", "covered",
-                           "uncovered", "total", "owner", "releaser", "??????",
-                           "option type"])
-
-frame[frame["stock"]=="PETR"].sort_values(by=["exercise date", "series"])
-frame.filter(["stock", "exercise date", "series", "strike price"])
-frame["call"] = frame["series"].str.get(4) < "L"
-
+from scipy.stats import norm
+from bs4 import BeautifulSoup
 
 
 def implied_volatility(target_value, call, s, k, t, r):
@@ -49,3 +37,38 @@ def bs(call, s, k, t, r, v, q=0.0) -> float:
 def vega(s, k, t, r, v) -> float:
     d1 = (math.log(s / k) + (r + 0.5 * v**2) * t) / (v * math.sqrt(t))
     return s * math.sqrt(t) * norm.pdf(d1)
+
+
+frame = pd.read_csv("csv/SI_D_ROPC.txt", sep="|", header=None, skiprows=1,
+                    parse_dates=[3], 
+                    names=["?", "stock", "stock type", "exercise date", "??", "???",
+                           "series", "????", "strike price", "?????", "covered",
+                           "uncovered", "total", "owner", "releaser", "??????",
+                           "option type"])
+
+frame[frame["stock"]=="PETR"].sort_values(by=["exercise date", "series"])
+frame.filter(["stock", "exercise date", "series", "strike price"])
+frame["call"] = frame["series"].str.get(4) < "L"
+
+frame['implied volatility'] = frame.apply(lambda x: implied_volatility(x['value_1'],
+                                                                       x['call'],
+                                                                       29.85,
+                                                                       x['strike price'],
+                                                                       x['exercise date'],
+                                                                       0.065), axis=1)
+
+with open("html/Opções de compra | Valor Econômico.html") as f:
+    html_string = f.read()
+soup = BeautifulSoup(html_string, 'lxml')
+table = soup.find_all('table')[0]
+
+new_table = pd.DataFrame(columns=range(0,2), index = [0]) # I know the size 
+
+row_marker = 0
+for row in table.find_all('tr'):
+    column_marker = 0
+    columns = row.find_all('td')
+    for column in columns:
+        new_table.iat[row_marker,column_marker] = column.get_text()
+        column_marker += 1
+
